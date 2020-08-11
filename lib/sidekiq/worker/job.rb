7d7092cbc072d7f7f9bc105cb5c5fd8699c0e938
andxors <andxors@gmail.com>
2020-08-11 11:34:36 +0400

    Method for creating a job from a hash

--- lib/sidekiq/worker/job.rb
+++ lib/sidekiq/worker/job.rb
@@ -9,16 +9,30 @@ module Sidekiq
       # @attr_reader queue_name  [String]
       # @attr_reader id          [String]
       # @attr_reader args        [Array]
-      # @attr_reader created_at  [Integer]
-      # @attr_reader enqueued_at [Integer]
+      # @attr_reader created_at  [Float]
+      # @attr_reader enqueued_at [Float]
       attr_reader :worker_name, :queue_name, :id, :args, :created_at, :enqueued_at
+
+      class << self
+        # @param attributes [Hash]
+        def new_from_payload(attributes)
+          new(
+            worker_name: attributes.fetch('class'),
+            queue_name:  attributes.fetch('queue'),
+            id:          attributes.fetch('jid'),
+            args:        attributes.fetch('args'),
+            created_at:  attributes.fetch('created_at'),
+            enqueued_at: attributes.fetch('enqueued_at')
+          )
+        end
+      end
 
       # @param worker_name [String]
       # @param queue_name  [String]
       # @param id          [String]
       # @param args        [Array]
-      # @param created_at  [Integer]
-      # @param enqueued_at [Integer]
+      # @param created_at  [Float]
+      # @param enqueued_at [Float]
       def initialize(worker_name:, queue_name:, id:, args:, created_at:, enqueued_at:)
         @worker_name = worker_name
         @queue_name  = queue_name
