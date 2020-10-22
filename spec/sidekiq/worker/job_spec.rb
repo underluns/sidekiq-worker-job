47b8decbcdb08e6196991c5a7dfd3520b7b68437
andxors <andxors@gmail.com>
2020-10-22 15:25:50 +0400

    Version 0.4.0

--- spec/sidekiq/worker/job_spec.rb
+++ spec/sidekiq/worker/job_spec.rb
@@ -1,7 +1,7 @@
 describe Sidekiq::Worker::Job do
   describe '.version' do
     it 'has a version number' do
-      expect(described_class::VERSION).to eq('0.3.0')
+      expect(described_class::VERSION).to eq('0.4.0')
     end
   end
 end
